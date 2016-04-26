badb1cbc187a2ad8361a8e17e6a44fa73c034795
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2016-04-26 11:01:25 +0100

    .

--- src/test/resources/predictions_analysis.r
+++ src/test/resources/predictions_analysis.r
@@ -9,11 +9,19 @@ test <- read.csv('data_booked/train_booked_sample_b.csv')
 #analyze mapk
 stat <- sqldf('select srch_destination_id,count(*) as c from train  group by srch_destination_id order by c')
 
-test <- merge(test,stat,all.x=T)
-test$c[is.na(test$c)] <- 0
-test <- merge(test,p,by='row.names')
+#test <- merge(test,p,by='row.names')
+test$mapk <- p$mapk
+test$p1 <- p$p1
+test$r1 <- p$r1
+
 test$mapk_svm <- p_svm$mapk
 test$p1_svm <- p_svm$p1
+test$r1_svm <- p_svm$r1
+
+test <- merge(test,stat,all.x=T)
+
+test$c[is.na(test$c)] <- 0
+test  <- subset(test,p1 !=1 & p1_svm !=1)
 
 ggplot() + geom_smooth(aes(col='mapk',x=b$c,y=b$mapk)) +
   coord_cartesian(ylim=c(-0.1,1.1),xlim=c(0,400))
