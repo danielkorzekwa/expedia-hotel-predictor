511bab0dab910e9efd1e4d748af30fdc9391a6f9
Daniel Korzekwa <daniel.korzekwa@gmail.com>
2016-05-06 17:47:27 +0100

    .

--- src/main/scala/expedia/model/marketdest/MarketDestPredictionModel.scala
+++ src/main/scala/expedia/model/marketdest/MarketDestPredictionModel.scala
@@ -12,10 +12,10 @@ import expedia.model.svm.loadClusterProbsByDestMap
 import expedia.model.svm.SVMPredictionModel
 
 case class MarketDestPredictionModel(clusterProbsByUser: Map[Tuple2[Int, Int], DenseVector[Float]],
-                                   clusterProbByDestMap: Map[Int, DenseVector[Float]],
-                                   clusterProbByDestMapSVM: Map[Int, DenseVector[Float]],
-                                   clusterProbMap: DenseVector[Float],
-                                   clusterStatByContinentMapNoPrior: Map[Int, DenseVector[Float]]) extends LazyLogging {
+                                     clusterProbByDestMap: Map[Int, DenseVector[Float]],
+                                     clusterProbByDestMapSVM: Map[Int, DenseVector[Float]],
+                                     clusterProbMap: DenseVector[Float],
+                                     clusterStatByContinentMapNoPrior: Map[Int, DenseVector[Float]]) extends LazyLogging {
 
   /**
    * @param data [user_id,dest]
@@ -23,13 +23,12 @@ case class MarketDestPredictionModel(clusterProbsByUser: Map[Tuple2[Int, Int], D
    */
   def predict(userId: Int, destId: Int, continent: Int): DenseVector[Float] = {
 
-   def userProbDefault(destId: Int): DenseVector[Float] = {
+    def userProbDefault(destId: Int): DenseVector[Float] = {
       clusterProbByDestMap.getOrElse(destId, clusterProbByDestMap.getOrElse(destId, clusterProbByDestMapSVM.getOrElse(destId, clusterStatByContinentMapNoPrior.getOrElse(continent, clusterProbMap))))
     }
 
     val userProb = clusterProbsByUser.getOrElse((destId, userId), userProbDefault(destId))
     userProb
-
 
   }
 
